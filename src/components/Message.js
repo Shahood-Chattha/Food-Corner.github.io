e78410f08e6061a058da1f997e5624ed768126ba
Shahood_Chattha <chatthashahood309@gmail.com>
2023-05-15 03:05:22 +0500

    push

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,45 +1,68 @@
-import React, { useEffect, useRef } from "react";
-import { useSelector, useDispatch } from 'react-redux';
-import "./operatorChatBoxStyle.css";
+import React, { useEffect } from "react";
+import { useDispatch } from 'react-redux';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckDouble, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { useLocation } from 'react-router-dom';
 
 import { deleteInvalidMessages } from '../features/chat/chatOperatorSlice';
-import Message from "./Message";
-import SendMessage from "./SendMessage";
-import { fetchMessages } from '../features/chat/chatslice';
+import { deleteUserMessage } from '../features/chat/chatOperatorSlice';
+import { auth } from "../firebase";
 
-const OperatorChatBox = ({ chatId }) => {
-  const scroll = useRef();
+const Message = ({ message, chatId }) => {
   const dispatch = useDispatch();
-  const messages = useSelector((state) => state.chat.messages);
+  const location = useLocation();
+  const [user] = useAuthState(auth);
+  const time = new Date(message.timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
   useEffect(() => {
     dispatch(deleteInvalidMessages(chatId));
   }, [dispatch, chatId]);
-  
-  useEffect(() => {
-    dispatch(fetchMessages(chatId));
-  }, [dispatch, chatId]);
 
-  useEffect(() => {
-    // Scroll to the bottom of the messages wrapper
-    scroll.current.scrollIntoView({ behavior: "smooth" });
-  }, [messages]);
+  let userSeenIcon;
+  if (message.senderId === user.uid) {
+    if (!message.senderId === user.uid && (message.userIISeen || message.userISeen)) {
+      userSeenIcon = <FontAwesomeIcon icon={faCheckDouble} style={{color: 'blue'}} />;
+    } else {
+      userSeenIcon = <FontAwesomeIcon icon={faCheckDouble} style={{color: 'grey'}} />;
+    }
+  } else {
+    userSeenIcon = null;
+  }
+
+  const deleteMessage = (messageId) => {
+    dispatch(deleteUserMessage({ messageId, chatId }))
+  };
 
   return (
-    <div className="messages-wrapper overflow position-relative" style={{ height: "calc(100vh - 50px)", width: "100%" }}>
-      <div className="overflow-y-auto px-2" style={{ maxHeight: "calc(100vh - 200px)", width: "100%" }}>
-        {messages?.map((message) => (
-          <Message key={message.id} message={message} chatId={chatId}/>
-        ))}
-        <span ref={scroll}></span>
-      </div>
-      <div className="d-flex justify-content-center position-absolute bottom-0 start-50 translate-middle-x w-100 mb-2">
-        <div className="container mx-auto">
-          <SendMessage chatId={chatId} />
+    <div
+      className={`chat-bubble p-2 px-3 ${message.senderId === user.uid ? "right" : ""}`}>
+      <img
+        className="chat-bubble__left"
+        src={message.avatar}
+        alt="user avatar"
+      />
+      <div className="chat-bubble__right">
+        <p className="user-name">{message.name}</p>
+        <p className="user-message mb-0">{message.text}</p>
+        <div className="d-flex align-items-center">
+          {location.pathname === "/operatorchat" && (
+            <div className="d-flex flex-grow-1 justify-content-start">
+              <FontAwesomeIcon
+                icon={faTrash}
+                style={{ color: 'grey', cursor: 'pointer' }}
+                onClick={() => deleteMessage(message.id)}
+              />
+            </div>
+          )}
+          <div className="d-flex justify-content-end">
+            <p className="message-time mb-0 mr-1">{time}</p>
+            {userSeenIcon}
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default OperatorChatBox;
+export default Message;
